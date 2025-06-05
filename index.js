c07affb0b9a808eb74d3265a8c89f42f61febf39
AlexPrincipal <chrisjaog@gmail.com>
2025-06-05 02:45:43 -0700

    Habilitar Apollo Sandbox en producción

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
+const {
+  ApolloServerPluginLandingPageProductionDefault
+} = require('@apollo/server/plugin/landingPage/default');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
@@ -10,35 +13,39 @@ const userTypeDefs = require('./schemas/userSchema');
 const facturaTypeDefs = require('./schemas/facturaSchema');
 
 const facturaResolvers = require('./controllers/facturaController');
-const userResolver = require('./controllers/userController')
+const userResolver = require('./controllers/userController');
 const productResolver = require('./controllers/productController');
 
-const resolvers = [userResolver, facturaResolvers, productResolver];
-
 const typeDefs = [productTypeDefs, userTypeDefs, facturaTypeDefs];
+const resolvers = [userResolver, facturaResolvers, productResolver];
 
 const startServer = async () => {
   const app = express();
 
-  // Conectar a MongoDB (omitido temporalmente)
-  
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ Conectado a MongoDB');
   } catch (err) {
     console.error('❌ Error conectando a MongoDB:', err.message);
   }
-  
 
-  // Apollo Server
-  const server = new ApolloServer({ typeDefs, resolvers });
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    introspection: true,
+    plugins: [
+      ApolloServerPluginLandingPageProductionDefault({ embed: true }) // 👈 Aquí el sandbox
+    ]
+  });
+
   await server.start();
 
   app.use(cors());
   app.use(express.json());
 
-  // Middleware GraphQL en /graphql
-  app.use('/graphql', expressMiddleware(server, { context: async ({ req }) => ({ req })}));
+  app.use('/graphql', expressMiddleware(server, {
+    context: async ({ req }) => ({ req })
+  }));
 
   const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => {
